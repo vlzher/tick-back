34754ef2d6465317c59c8241ffd4d307654c796c
Vladislav Zherebyatev <vlzher>
2024-05-08 16:06:30 +0200

    fdfd

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const WebSocket = require('ws');
+const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 let games = [];
-let usernamesQueue = [];
 const userWebSocketMap = {};
 const wss = new WebSocket.Server({ port: 8080 });
+const CLIENT_ID = "1nbjcn2p356d6eb8760os6qo1h"
+const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({
+    region: 'us-east-1'
+});
 
 wss.on('listening', () => {
     console.log('WebSocket server started and listening on port 8080');
@@ -12,20 +16,30 @@ wss.on('listening', () => {
 
 wss.on('connection', function connection(ws) {
     console.log('WebSocket connection established');
-
-    ws.on('message', function incoming(message) {
+    ws.send(JSON.stringify({ type: 'connected' }));
+    ws.on('message', async function incoming(message) {
         console.log('Received message:', message);
 
         const data = JSON.parse(message);
+        if(!data) return;
         switch (data.type) {
+            case 'register':
+                await handleRegister(ws, data);
+                break;
             case 'login':
-                handleLogin(ws, data);
+                await handleLogin(ws, data);
+                break;
+            case 'refresh_token':
+                await refreshToken(ws, data);
+                break;
+            case 'start_game':
+                await startGameUser(ws,data);
                 break;
             case 'gameMove':
-                handleGameMove(ws, data);
+                await handleGameMove(ws, data);
                 break;
             case 'gameEnd':
-                handleGameEnd(ws, data);
+                await handleGameEnd(ws, data);
                 break;
             default:
                 console.log('Unknown message type:', data.type);
@@ -37,50 +51,153 @@ wss.on('connection', function connection(ws) {
     });
 });
 
-function getTwoRandomNumbersInRange(min, max) {
-    let num1 = Math.floor(Math.random() * (max - min + 1)) + min;
-    let num2 = Math.floor(Math.random() * (max - min + 1)) + min;
-    while (num2 === num1) {
-        num2 = Math.floor(Math.random() * (max - min + 1)) + min;
+
+async function refreshToken(ws, data) {
+    console.log('Handling refresh token request:', data);
+    const { refreshToken } = data;
+    try {
+        const params = {
+            AuthFlow: 'REFRESH_TOKEN_AUTH',
+            ClientId: CLIENT_ID,
+            AuthParameters: {
+                REFRESH_TOKEN: refreshToken
+            }
+        };
+
+
+        const authData = await cognitoIdentityServiceProvider.initiateAuth(params).promise();
+        console.log('Access token refreshed successfully:', authData);
+        const accessToken = authData.AuthenticationResult.AccessToken;
+        const newRefreshToken = authData.AuthenticationResult.RefreshToken;
+        ws.send(JSON.stringify({ type: 'refresh_success', accessToken, refreshToken: newRefreshToken }));
     }
-    return [num1, num2];
+    catch (error) {
+        console.log('Refresh failed:', error);
+        ws.send(JSON.stringify({ type: 'refresh_failed' }));
+    }
 }
 
-function handleLogin(ws, data) {
-    console.log('Handling login request:', data);
+async function checkAccessToken(ws,accessToken) {
+    try {
+        const params = {
+            AccessToken: accessToken
+        };
 
-    const { username } = data;
-    if (!usernamesQueue.includes(username)) {
-        console.log('Username is available');
-        userWebSocketMap[username] =  ws;
-    } else {
+        await cognitoIdentityServiceProvider.getUser(params).promise();
+        return true;
+    } catch (error) {
+        ws.send(JSON.stringify({ type: 'access_token_invalid' }));
+        return false;
+    }
+
+}
+
+async function startGameUser(ws, data){
+    console.log('Handling start game request:', data)
+    const {username,access_token} = data;
+    if(!username) return;
+    const valid = await checkAccessToken(ws,access_token);
+    if(!valid) return;
+    userWebSocketMap[username] = ws;
+    const usernames = Object.keys(userWebSocketMap);
+    console.log('Usernames:', usernames)
+    if(usernames.length < 2) return;
+    const [username1, username2] = usernames.splice(0, 2);
+    console.log(usernames)
+
+    const gameID = uuidv4();
+    games.push({ gameID, username1, username2 });
+    console.log(games)
+
+    userWebSocketMap[username1].send(JSON.stringify({ type: 'game_start', gameID, isX: true }));
+    userWebSocketMap[username2].send(JSON.stringify({ type: 'game_start', gameID, isX: false }));
+    delete userWebSocketMap[username1];
+    delete userWebSocketMap[username2];
+}
+
+async function handleRegister(ws, data) {
+    console.log('Handling register request:', data);
+    const { username, email, password } = data;
+
+    if (userWebSocketMap.hasOwnProperty(username)) {
         console.log('Username is already in use');
+        ws.send(JSON.stringify({ type: 'register_failed', error: 'Username is already in use' }));
         return;
     }
-    usernamesQueue.push(username);
-    if(usernamesQueue.length < 2) return;
-    const [firstNumber, secondNumber] = getTwoRandomNumbersInRange(0, usernamesQueue.length-1);
-    const username1 = usernamesQueue[firstNumber];
-    const username2 = usernamesQueue[secondNumber];
-    usernamesQueue = usernamesQueue.filter((_, index) => index !== firstNumber && index !== secondNumber);
-    const gameID = uuidv4();
-    games.push({gameID, username1, username2});
-    userWebSocketMap[username1].send(JSON.stringify({type: `gameStart`, gameID, isX: true}));
-    userWebSocketMap[username2].send(JSON.stringify({type: `gameStart`, gameID, isX: false}));
+    try {
+        const params = {
+            ClientId: CLIENT_ID,
+            Password: password,
+            Username: username,
+            UserAttributes: [
+                {
+                    Name: 'email',
+                    Value: email
+                }
+            ]
+        };
+
+        await cognitoIdentityServiceProvider.signUp(params).promise();
+
+        userWebSocketMap[username] = ws;
+
+        console.log('User registered successfully');
+        ws.send(JSON.stringify({ type: 'register_success', message: 'User registered successfully' }));
+    } catch (error) {
+        console.log('Registration failed:', error);
+        ws.send(JSON.stringify({ type: 'register_failed', error: 'Registration failed' }));
+    }
 }
 
-function handleGameMove(ws, data) {
+
+async function handleLogin(ws, data) {
+    console.log('Handling login request:', data);
+
+    const { username, password } = data;
+
+    try {
+        const authParams = {
+            AuthFlow: 'USER_PASSWORD_AUTH',
+            ClientId: CLIENT_ID,
+            AuthParameters: {
+                USERNAME: username,
+                PASSWORD: password
+            }
+        };
+
+        const authData = await cognitoIdentityServiceProvider.initiateAuth(authParams).promise();
+        const accessToken = authData.AuthenticationResult.AccessToken;
+        const refreshToken = authData.AuthenticationResult.RefreshToken;
+
+
+        ws.send(JSON.stringify({ type: 'login_success', accessToken: accessToken, refreshToken: refreshToken }));
+    } catch (error) {
+        console.error('Login failed:', error);
+        // Send an error response to the client
+        ws.send(JSON.stringify({ type: 'login_failed', error: 'Invalid username or password' }));
+    }
+}
+
+async function handleGameMove(ws, data) {
     console.log('Handling game move:', data);
+    const {access_token} = data;
+    const valid = await checkAccessToken(ws, access_token);
+    if(!valid) return;
 
     const {gameID, move, username} = data;
     const game = games.find(game => game.gameID === gameID);
     if(!game) return;
+
     const opponentUsername = game.username1 === username ?  game.username2 : game.username1;
     userWebSocketMap[opponentUsername].send(JSON.stringify({type: `move`, move: move}));
 }
 
-function handleGameEnd(ws, data) {
+async function handleGameEnd(ws, data) {
     console.log('Handling game end:', data);
+
+    const {access_token} = data;
+    const valid = await checkAccessToken(ws, access_token);
+    if(!valid) return;
 
     const {gameID} = data;
     games = games.filter(game => game.id !== gameID);
